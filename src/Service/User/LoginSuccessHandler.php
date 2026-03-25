c91374c1771e16e87ca7201ea56a6b6bbae2f92e
hamza030220 <hamza.slimani@esprit.tn>
2026-03-25 09:54:06 +0100

    Initial commit: FIRMA Symfony project

---
+++ src/Service/User/LoginSuccessHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service\User;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    public function __construct(private RouterInterface $router)
+    {
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
+    {
+        $user = $token->getUser();
+        $roles = $user->getRoles();
+
+        if (in_array('ROLE_ADMIN', $roles, true)) {
+            return new RedirectResponse($this->router->generate('admin_dashboard'));
+        }
+
+        return new RedirectResponse($this->router->generate('user_dashboard'));
+    }
+}
